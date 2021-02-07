98fd33dc4d9d306f0fe99793ab7007ff9f2fbff7
Andrew <andrew.casin@gmail.com>
2021-02-07 01:31:01 -0600

    Add validations to watch. Update to offer validations

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -3,4 +3,10 @@ class Watch < ActiveRecord::Base
     has_many :users, through: :userwatches
     belongs_to :genre 
     belongs_to :brand
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0}
+    validates :brand, presence: true
+    validates :genre, presence: true
+    validates :owner, presence: true
 end
