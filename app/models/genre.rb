b754f5fe02cdb415eb08fa0c028b44ad3a18768e
Andrew <andrew.casin@gmail.com>
2021-02-07 02:29:44 -0600

    Correct model typos

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ActiveRecord::Base
     has_many :watches
     has_many :brands, through: :watches 
     has_many :users, through: :watches
-    validates: :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
 end
