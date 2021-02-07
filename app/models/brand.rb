4974a6dd106adc02fc6b2bce510b3fea95efeaa4
Andrew <andrew.casin@gmail.com>
2021-02-06 20:09:05 -0600

    Create associations for Genre and Offer models

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,5 @@
 class Brand < ActiveRecord::Base
+    has_many :watches 
+    has_many :genres, through: :watches
+    has_many :users, through: :watches
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,5 @@
 class Genre < ActiveRecord::Base
+    has_many :watches
+    has_many :brands, through: :watches 
+    has_many :users, through: :watches
 end
