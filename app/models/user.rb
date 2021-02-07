91fb1f1206ef20c5411f86f90476818ebe69fd64
Andrew <andrew.casin@gmail.com>
2021-02-07 01:13:15 -0600

    Add validation for Brand

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
     has_many :initiated_offers, class_name: "Offer", foreign_key: "sender_id" 
     has_many :received_offers, class_name: "Offer", foreign_key: "receiver_id" 
     has_secure_password
+    validates :password, length: {in: 10..100}, confirmation: true#, on: :create
 end
