c0761bc1275b3e7c37f90c020829a2b4b4e63799
Andrew <andrew.casin@gmail.com>
2021-02-06 20:02:43 -0600

    Create associations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    has_many :userwatches
+    has_many :watches, through: :userwatches
+    has_many :genres, through: :watches
+    has_many :brands, through: :watches
+    has_many :offers 
 end
