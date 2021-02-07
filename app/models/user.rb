0024857b37ede01892a03e2c5e82e2af9a8abdd7
Andrew <andrew.casin@gmail.com>
2021-02-07 01:21:45 -0600

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
     has_many :received_offers, class_name: "Offer", foreign_key: "receiver_id" 
     has_secure_password
     validates :password, length: {in: 10..100}, confirmation: true#, on: :create
+    validates :username, uniqueness: true, length: {in: 3..30}, exclusion: {in: %w(admin superadmin user)}
+    validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
 end
