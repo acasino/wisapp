6f305c26afee9dffa33f8dd695de3047b81f2cc9
Andrew <andrew.casin@gmail.com>
2021-02-07 22:41:17 -0600

    Create users get and post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,57 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
-  end
-
   # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  get "/signup" do
+    erb :signup
   end
 
   # POST: /users
-  post "/users" do
-    redirect "/users"
+  post "/signup" do
+    user = User.create(params["user"])
+    if user.valid?
+      flash[:success] = "Successfully created new user."
+      session["user_id"] = user.id
+      redirect '/users/show' ###sluggable###
+    else
+      flash[:error] = user.errors.full_messages.first
+      redirect '/signup'
+    end
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  # # GET: /users
+  # get "/users" do
+  #   erb :"/users/index.html"
+  # end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  # # GET: /users/new
+  # get "/users/new" do
+  #   erb :"/users/new.html"
+  # end
+
+  # # POST: /users
+  # post "/users" do
+  #   redirect "/users"
+  # end
+
+  # # GET: /users/5
+  # get "/users/:id" do
+  #   erb :"/users/show.html"
+  # end
+
+  # # GET: /users/5/edit
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
+
+  # # PATCH: /users/5
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
+
+  # # DELETE: /users/5/delete
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
 end
