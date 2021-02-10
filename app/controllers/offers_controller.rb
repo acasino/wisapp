9c0343a6a82b9353c3a38f10a66c5872c15af939
Andrew <andrew.casin@gmail.com>
2021-02-10 00:22:16 -0600

    Update to offer controller and show view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,6 +7,7 @@ class OffersController < ApplicationController
 
   # GET: /offers/new
   get "/offers/new" do
+    
     erb :"/offers/new.html"
   end
 
@@ -15,23 +16,23 @@ class OffersController < ApplicationController
     redirect "/offers"
   end
 
-  # GET: /offers/5
-  get "/offers/:id" do
-    erb :"/offers/show.html"
-  end
+  # # GET: /offers/5
+  # get "/offers/:id" do
+  #   erb :"/offers/show.html"
+  # end
 
-  # GET: /offers/5/edit
-  get "/offers/:id/edit" do
-    erb :"/offers/edit.html"
-  end
+  # # GET: /offers/5/edit
+  # get "/offers/:id/edit" do
+  #   erb :"/offers/edit.html"
+  # end
 
-  # PATCH: /offers/5
-  patch "/offers/:id" do
-    redirect "/offers/:id"
-  end
+  # # PATCH: /offers/5
+  # patch "/offers/:id" do
+  #   redirect "/offers/:id"
+  # end
 
-  # DELETE: /offers/5/delete
-  delete "/offers/:id/delete" do
-    redirect "/offers"
-  end
+  # # DELETE: /offers/5/delete
+  # delete "/offers/:id/delete" do
+  #   redirect "/offers"
+  # end
 end
