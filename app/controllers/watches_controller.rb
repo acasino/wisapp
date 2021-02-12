3508698b7ae26bb82b8514cce77615eb78e23aa9
Andrew <andrew.casin@gmail.com>
2021-02-11 23:14:00 -0600

    Add additional logic to watch creation

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,22 +1,19 @@
 class WatchesController < ApplicationController
-
   # GET: /watches
   get "/watches" do
     @watches = Watch.all
-
     erb :"/watches/index.html"
   end
-
   # GET: /watches/new
   get "/watches/new.html" do
     erb :"/watches/new.html"
   end
-
   # POST: /watches
   post "/watches" do
     wristwatch = Watch.create(params["watch"]) 
-    if wristwatch.valid? 
-      Userwatch.create(user: current_user, watch: wristwatch)
+    if wristwatch.valid?
+      # binding.pry
+      Userwatch.create(user: current_user, watch: wristwatch) if !current_user.watches.include?(wristwatch)
       flash[:success] = "Successfully created new watch."
       redirect '/users/profile.html' 
     else
@@ -24,7 +21,6 @@ class WatchesController < ApplicationController
       redirect '/watches/new.html'
     end
   end
-
   # # GET: /watches/5
   get "/watches/:id" do
     @user = current_user
@@ -34,13 +30,11 @@ class WatchesController < ApplicationController
     # @id = @watch.userwatches.where
     erb :"/watches/show.html"
   end
-
   # GET: /watches/5/edit
   get "/watches/:id/edit.html" do
     @watch = Watch.find_by_id(params[:id])
     erb :"/watches/edit.html"
   end
-
   # PATCH: /watches/5
   patch "/watches/:id" do
     @watch = Watch.find_by_id(params[:id])
@@ -50,18 +44,17 @@ class WatchesController < ApplicationController
     # @watch.save 
     redirect "/watches/#{@watch.id}"
   end
-
   # DELETE: /watches/5/delete
   delete "/watches/:id" do
-    @user = current_user
     if logged_in?
-      @watch = Watch.find_by_id(params[:id])
-      @watch.delete
-      flash[:message] = "#{@watch.name} Deleted"
+      watch = Watch.find_by_id(params[:id])
+      watch.delete
+      Userwatch.where("watch_id = ?", watch.id).delete_all
+      flash[:message] = "#{watch.name} Deleted"
       redirect "/users/profile.html"
     else
-      flash[:message] = "Unable To Delete #{@watch.name}"
-      redirect "/#{@watch.id}"
+      flash[:message] = "Unable To Delete #{watch.name}"
+      redirect "/#{watch.id}"
     end
   end
 end
